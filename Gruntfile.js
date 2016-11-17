2f7808416480f88b905837f6287856de4d4a9aaf
Daniel Park <park@ytel.com>
2016-11-17 14:43:53 -0800

    gruntfile setup

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+    // Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        clean: {
+
+        },
+        copy: {
+
+        },
+        ngAnnotate: {
+
+        },
+        cssmin: {
+
+        },
+        concat: {
+
+        },
+        
+    })
+}
