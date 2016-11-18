2db3c81fc8a3a4b3e16fb5af6a91b1f2ef1c317d
Daniel Park <park@ytel.com>
2016-11-18 14:24:13 -0800

    Gruntfile.js update

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,28 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         clean: {
-
+            startBuild: "./tmp",
+            finishBuild: "./tmp"
         },
         copy: {
-
+            mainFiles: {
+                files: [{
+                    cwd: 'bower_components/font-awesome/fonts',
+                    src: '**/*',
+                    dest: './dist/fonts',
+                    expand: true
+                }]
+            }
         },
         ngAnnotate: {
+            options: {
+                singleQuotes: true
+            },
+            app: {
+                files: {
 
+                }
+            }
         },
         cssmin: {
 
@@ -18,5 +33,24 @@ module.exports = function(grunt) {
 
         },
         
-    })
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-ng-annotate");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+
+    grunt.registerTask('default', [
+        'copy',
+        'clean:startBuild',
+        'ngAnnotate',
+        'cssmin',
+        'ngtemplates',
+        'concat',
+        'uglify',
+        'clean:finishBuild'
+    ]);
 }
